f85295cbb3cf2c9990c37184c5ddd7979c03d1f6
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-05-29 19:44:43 +0530

    New card grid with HTML & CSS

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,17 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp';
 import HeroComp from './Components/Hero/HeroComp';
 import GridComp from './Components/GridComp';
-import Test from './Components/Test';
 
 function App() {
   return (
     <div className="App">
       <NavbarComp/>
-     <HeroComp/><br></br><br></br><br></br>
-     <GridComp/><br></br>
-     <Test/>
-     
-                
+     <HeroComp/> 
+     <GridComp/>
     </div>
   );
 }
