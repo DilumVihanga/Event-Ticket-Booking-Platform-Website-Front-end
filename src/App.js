3ac34e3866fe6cc167d960ffa9d936b6aa87f2ba
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-02 14:56:16 +0530

    horizontal card grid added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavComp from './Components/Navbar/NavComp';
 import HeroComp from './Components/Hero/HeroComp';
 import CardGrid from './Components/CardGrid/CardGrid';
+import HorizontalcardComp from './Components/Horizontalcard/HorizontalcardComp';
 
 function App() {
   return (
     <div className="App">
       <NavComp/>
-     <HeroComp/><br></br><br></br><br></br>
+     <HeroComp/><br></br><br></br>
+     <HorizontalcardComp/> <br></br>
           <CardGrid/>
      
                 
---
+++ src/Components/Horizontalcard/HorizontalcardComp.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import "../Horizontalcard/HorizontalcardComp.css"
+
+export default function HorizontalcardComp() {
+  return (
+    <div>
+        <div class="container">
+  <div class="card dark">
+    <img src="https://codingyaar.com/wp-content/uploads/chair-image.jpg" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <div class="text-section">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card's
+          content.</p>
+      </div>
+      <div class="cta-section">
+        <div>$129.00</div>
+        <a href="#" class="btn btn-light">Buy Now</a>
+      </div>
+    </div>
+  </div>  
+</div>
+
+<div class="container">
+  <div class="card dark">
+    <img src="https://codingyaar.com/wp-content/uploads/chair-image.jpg" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <div class="text-section">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card's
+          content.</p>
+      </div>
+      <div class="cta-section">
+        <div>$129.00</div>
+        <a href="#" class="btn btn-light">Buy Now</a>
+      </div>
+    </div>
+  </div>  
+</div>
+
+ </div>
+  )
+}
