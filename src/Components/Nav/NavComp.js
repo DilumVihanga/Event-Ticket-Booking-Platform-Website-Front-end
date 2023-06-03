33931aabb053545b5378ef8aac9c167b7303a8b3
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-03 21:49:13 +0530

    Menu updated

--- src/Components/Nav/NavComp.js
+++ src/Components/Nav/NavComp.js
@@ -13,10 +13,12 @@ export default function NavComp() {
     </div>
     <nav>
         <ul>
-           <li><a href="#">Articles</a></li>
-           <li><a href="#">Projects</a></li>
+           <li><a href="#">Home</a></li>
+           <li><a href="#">Upcoming Events</a></li>
            <li><a href="#">About</a></li> 
             <li><a href="#">Contact</a></li>
+            <li><a href="#">Cart</a></li>
+            <li><a href="#">Checkout</a></li>
        </ul>
    </nav>
 </div>
