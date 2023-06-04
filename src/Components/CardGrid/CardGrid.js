52544112a39de1c835d7f4b4427697c762cc52c3
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-04 23:46:39 +0530

    Card image updated

--- src/Components/CardGrid/CardGrid.js
+++ src/Components/CardGrid/CardGrid.js
@@ -40,7 +40,9 @@ export default function CardGrid() {
     </li>
     <li class="cards_item">
       <div class="card">
-        <div class="card_image"><img src="https://picsum.photos/500/300/?image=14"/></div>
+        <div class="card_image">
+          <img src="https://thebendwi.org/wp-content/uploads/2023/03/Radio-Gaga_LiveMusic-at-The-Bend-Theater.jpg" />
+          </div>
         <div class="card_content">
           <h2 class="card_title">Card Grid Layout</h2>
           <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
