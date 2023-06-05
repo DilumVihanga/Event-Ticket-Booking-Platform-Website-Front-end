bc3236efa7708f3bd6fc7f34b10d09b642097e3a
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-05 20:27:34 +0530

    reuseble component added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from 'react'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Contact from './Pages/Contact';
 import Upcomingevents from './Pages/Upcomingevents';
 import Home from './Pages/Home';
-
-import React from 'react'
+import NavComp from './Components/Nav/NavComp';
+import FooterComp from './Components/Footer/FooterComp';
 
 export default function () {
   return (
     <div>
-
+      <NavComp/>
       <BrowserRouter>
       <Routes>
         <Route index element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/upcomingevents" element={<Upcomingevents/>}/>
         </Routes>      
       </BrowserRouter>
+      <FooterComp/>
       
     </div>
   )
