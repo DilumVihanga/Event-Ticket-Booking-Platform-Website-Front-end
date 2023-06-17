9df4bcb70a1d92305a35b0d0b6863ab48f07d268
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-17 23:35:40 +0530

    user dashboard (sidebar) route

---
+++ src/Pages/SidebarUSER.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideNavBar from '../Components/SideNavBar/SideNavBar'
+
+export default function SidebarUSER() {
+  return (
+    <div>
+        <SideNavBar/>
+    </div>
+  )
+}
